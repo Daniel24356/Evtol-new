c28d162486573ad63f62b5d456dd2d50259e4d1f
Daniel24356 <igwedaniel128@gmail.com>
2025-09-11 21:47:41 -0700

    uncommented all.tsx 1

--- src/Admin/All.jsx
+++ src/Admin/All.jsx
@@ -1,22 +1,22 @@
-// import AdminDashboard from "./dashboard"
-// import Sidebar from "./Sidebar"
-// import './all.css'
-// import { Outlet } from "react-router-dom"
+import AdminDashboard from "./dashboard"
+import Sidebar from "./Sidebar"
+import './all.css'
+import { Outlet } from "react-router-dom"
 
 
-// const All = () => {
-//     return(
-//         <>
-//         <div className="all-dash">
-//         <Sidebar/>
-//         <div>
-//         <Outlet/>
-//         </div>
-//         {/* <AdminDashboard/> */}
-//         </div>
+const All = () => {
+    return(
+        <>
+        <div className="all-dash">
+        <Sidebar/>
+        <div>
+        <Outlet/>
+        </div>
+        {/* <AdminDashboard/> */}
+        </div>
        
-//         </>
-//     )
-// }
+        </>
+    )
+}
 
-// export default All
+export default All
