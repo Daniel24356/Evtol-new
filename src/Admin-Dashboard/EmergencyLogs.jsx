231ac2eefd70145d4260b203c1ff9eeea488a37b
Daniel24356 <igwedaniel128@gmail.com>
2025-09-11 22:04:49 -0700

    uncommented emergencylog.jsx

--- src/Admin-Dashboard/EmergencyLogs.jsx
+++ src/Admin-Dashboard/EmergencyLogs.jsx
@@ -1,40 +1,40 @@
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-// import { Line } from "react-chartjs-2";
-// import "chart.js/auto";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Line } from "react-chartjs-2";
+import "chart.js/auto";
 
-// const EmergencyLogs = () => {
-//     const [logs, setLogs] = useState([]);
+const EmergencyLogs = () => {
+    const [logs, setLogs] = useState([]);
   
-//     useEffect(() => {
-//       axios.get("http://localhost:3020/api/v1/fleet/emergency-logs")
-//         .then((res) => setLogs(res.data))
-//         .catch((err) => console.error("Error fetching emergency logs:", err));
-//     }, []);
+    useEffect(() => {
+      axios.get("http://localhost:3020/api/v1/fleet/emergency-logs")
+        .then((res) => setLogs(res.data))
+        .catch((err) => console.error("Error fetching emergency logs:", err));
+    }, []);
     
-//     return (
-//       <div>
-//         <h2>Emergency Landings & Failures</h2>
-//         <table>
-//           <thead>
-//             <tr>
-//               <th>eVTOL</th>
-//               <th>Reason</th>
-//               <th>Timestamp</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {logs.map((log) => (
-//               <tr key={log.id}>
-//                 <td>{log.serialNumber}</td>
-//                 <td>{log.reason}</td>
-//                 <td>{new Date(log.timestamp).toLocaleString()}</td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//       </div>
-//     );
-//   };
+    return (
+      <div>
+        <h2>Emergency Landings & Failures</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>eVTOL</th>
+              <th>Reason</th>
+              <th>Timestamp</th>
+            </tr>
+          </thead>
+          <tbody>
+            {logs.map((log) => (
+              <tr key={log.id}>
+                <td>{log.serialNumber}</td>
+                <td>{log.reason}</td>
+                <td>{new Date(log.timestamp).toLocaleString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
   
-//   export default EmergencyLogs
+  export default EmergencyLogs
