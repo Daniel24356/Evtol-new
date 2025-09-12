72e2d1c53c7706604a87e4940f381c0b3018b7a5
Daniel24356 <igwedaniel128@gmail.com>
2025-09-11 21:49:39 -0700

    commented dashboard.tsx

--- src/Admin/dashboard.jsx
+++ src/Admin/dashboard.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import OverviewCards from "./overviewcCards";
-import LiveMap from "./Livemap";
-import FleetAnalytics from "./FleetAnalytics";
-import EvtolListTable from "./EvtolListTable";
-import Sidebar from "./Sidebar";
-import './dashboard.css'
+// import React from "react";
+// import OverviewCards from "./overviewcCards";
+// import LiveMap from "./Livemap";
+// import FleetAnalytics from "./FleetAnalytics";
+// import EvtolListTable from "./EvtolListTable";
+// import Sidebar from "./Sidebar";
+// import './dashboard.css'
 
-const AdminDashboard = () => {
-  return (
-    <>
-        <div className="dash-view">
-          {/* Overview Cards */}
-          <OverviewCards/>
-          {/* Live Map Tracking */}
-          {/* <LiveMap/> */}
-          {/* eVTOLs Table */}
-          <EvtolListTable/>
-          {/* Fleet Analytics Section */}
-          <FleetAnalytics/>
-        </div>
-    </>
-  );
-};
+// const AdminDashboard = () => {
+//   return (
+//     <>
+//         <div className="dash-view">
+//           {/* Overview Cards */}
+//           <OverviewCards/>
+//           {/* Live Map Tracking */}
+//           {/* <LiveMap/> */}
+//           {/* eVTOLs Table */}
+//           <EvtolListTable/>
+//           {/* Fleet Analytics Section */}
+//           <FleetAnalytics/>
+//         </div>
+//     </>
+//   );
+// };
 
-export default AdminDashboard;
+// export default AdminDashboard;
